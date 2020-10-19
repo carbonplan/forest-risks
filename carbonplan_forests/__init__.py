def8f0e8efc01afa02782a66c4c3ddf1a0bf95d1
Joseph Hamman <jhamman@ucar.edu>
2020-10-19 13:25:02 -0700

    explicit imports of submodules

--- carbonplan_forests/__init__.py
+++ carbonplan_forests/__init__.py
@@ -1,5 +1,7 @@
 from pkg_resources import DistributionNotFound, get_distribution
 
+from . import data, fit, load, plot, preprocess, setup  # noqa
+
 try:
     version = get_distribution(__name__).version
 except DistributionNotFound:  # pragma: no cover
