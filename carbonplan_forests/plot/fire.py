099fa061aba171ce87df898689af60c4fcf1538f
Oriana Chegwidden <orianac@uw.edu>
2021-01-13 07:50:37 +0000

    adding two plotting functions- one to package data nicely and one to plot a jaw-droppingly beautiful sliding chart

--- carbonplan_forests/plot/fire.py
+++ carbonplan_forests/plot/fire.py
@@ -1,4 +1,6 @@
 import altair as alt
+import pandas as pd
+import scipy as sp
 
 from . import carto, line
 
@@ -38,6 +40,33 @@ def monthly(data, data_var='monthly', projection='albersUsa', clim=None):
         chart &= row
 
     return chart.configure_view(strokeOpacity=0)
+
+
+def simple_map(data, clabel=None, projection='albersUsa', clim=None, cmap='reds'):
+    lat = data['lat'].values.flatten()
+    lon = data['lon'].values.flatten()
+    color = data.values.flatten()
+    inds = color > clim[0]
+
+    shape = data['lat'].shape
+    size = (300 / shape[0]) * (500 / shape[1]) * 0.9
+
+    row = alt.hconcat()
+
+    row |= carto(
+        lat=lat[inds],
+        lon=lon[inds],
+        color=color[inds],
+        clim=clim,
+        cmap=cmap,
+        clabel=clabel,
+        size=size,
+        width=500,
+        height=300,
+        projection=projection,
+    )
+
+    return row
 
 
 def summary(data, data_var='monthly', projection='albersUsa', clim=None):
@@ -126,3 +155,51 @@ def evaluation(data, model, data_var='vlf', model_var='prob', projection='albers
     )
 
     return chart
+
+
+def package_for_altair(fire, climate, label='value'):
+    data = fire.mean(dim=['x', 'y']).monthly.to_dataframe().rename(columns={'monthly': 'fire'})
+    data['temp'] = climate['tmax'].mean(dim=['x', 'y']).values
+    data['ppt'] = climate['ppt'].mean(dim=['x', 'y']).values
+    for variable in data.columns:
+        data[variable] = sp.stats.zscore(data[variable])
+    data['year'] = data.index.year
+    data['month'] = data.index.month
+    data = data.reset_index().drop(['time'], axis=1)
+    data = pd.melt(
+        data,
+        id_vars=['year', 'month'],
+        value_vars=['fire', 'temp', 'ppt'],
+        value_name=label,
+        ignore_index=False,
+    )
+    return data
+
+
+def multipanel_slider(data, year_limits, region_labels):
+
+    if len(region_labels) != 4:
+        raise Exception('SO SORRY! Four is the magic number- we only accept four regions right now')
+    slider = alt.binding_range(min=year_limits[0], max=year_limits[0], step=1)
+    select_year = alt.selection_single(
+        name="year", fields=['year'], bind=slider, init={'year': year_limits[0]}
+    )
+
+    base = (
+        alt.Chart(data)
+        .mark_line()
+        .encode(x='month:O', color='variable:N')
+        .add_selection(select_year)
+        .properties(width=200, height=100)
+        .transform_filter(select_year)
+    )
+    charts = {}
+    for region in region_labels:
+        charts[region] = base.encode(y='{}:Q'.format(region))
+    full_chart = (
+        charts[region_labels[0]]
+        | charts[region_labels[1]]
+        | charts[region_labels[2]]
+        | charts[region_labels[3]]
+    )
+    return full_chart
