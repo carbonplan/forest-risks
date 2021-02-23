0c08d64e528e7b5d1171ede8758b51515314caae
Oriana Chegwidden <orianac@uw.edu>
2021-02-23 05:07:15 +0000

    updating run script

--- scripts/fire.py
+++ scripts/fire.py
@@ -1,12 +1,12 @@
-import functools
+import os
 import sys
 import warnings
 
-import numpy as np
 import xarray as xr
 from tqdm import tqdm
 
-from carbonplan_forests import fit, load
+from carbonplan_forests import collect, fit, load, prepare, utils
+from carbonplan_forests.utils import get_store
 
 warnings.simplefilter('ignore', category=RuntimeWarning)
 
@@ -18,65 +18,109 @@ else:
     store = args[1]
 
 coarsen_fit = 4
-coarsen_predict = None
-coarsen_scale = 1 / coarsen_fit
+coarsen_predict = 4
 tlim = (1984, 2018)
-data_vars = ['ppt', 'tavg']
-fit_vars = ['ppt', 'tavg']
-
-
-def integrated_risk(da):
-    return 1 - functools.reduce(
-        lambda a, b: a * b, [1 - year for year in da.groupby('time.month').mean()]
-    )
+data_vars = ['tmean', 'ppt']  # "cwd", "pdsi", ]
+fit_vars = ['tmean', 'ppt']  # "cwd", "pdsi", ]
 
 
 print('[fire] loading data')
-mask = load.nlcd(store=store, classes='all', year=2001)
-groups = load.nftd(store=store, groups='all', coarsen=coarsen_fit, mask=mask, area_threshold=1500)
+mask = load.mask(store=store, year=2001)
+nftd = load.nftd(store=store, groups='all', coarsen=coarsen_fit, mask=mask, area_threshold=1500)
+nlcd = load.nlcd(store=store, year=2001, coarsen=coarsen_fit, mask=mask)
+
 climate = load.terraclim(
-    store=store, tlim=tlim, coarsen=coarsen_fit, data_vars=data_vars, mask=mask
+    store=store,
+    tlim=tlim,
+    coarsen=coarsen_fit,
+    variables=data_vars,
+    mask=mask,
+    sampling="monthly",
 )
 mtbs = load.mtbs(store=store, coarsen=coarsen_fit, tlim=tlim)
-mtbs['vlf'] = mtbs['vlf'] > 0
+mtbs = mtbs.assign_coords({'x': nftd.x, 'y': nftd.y})
+mtbs_test = mtbs * (nlcd.sel(band=[41, 42, 43, 90]).sum('band') > 0.1).values
+# @jeremy - do we still want this line?
+# mtbs['monthly'] = mtbs['monthly'] > 0
 
 print('[fire] fitting model')
-model = fit.fire(x=climate[fit_vars], y=mtbs['vlf'], f=groups)
+x, y = prepare.fire(climate, nftd, mtbs, add_local_climate_trends=True)
+x_z, x_mean, x_std = utils.zscore_2d(x)
+model = fit.hurdle(x_z, y, log=False)
+yhat = model.predict(x_z)
+prediction = collect.fire(yhat, mtbs)
 
+# what does this do?
 print('[fire] setting up evaluation')
-final_mask = load.nlcd(store=store, year=2016, coarsen=coarsen_predict, classes=[41, 42, 43, 90])
-final_mask.values = final_mask.values > 0.5
+
 ds = xr.Dataset()
 
 print('[fire] evaluating on training data')
-groups = load.nftd(
-    store=store, groups='all', mask=mask, coarsen=coarsen_predict, area_threshold=1500
-)
 climate = load.terraclim(
-    store=store, tlim=(2005, 2014), coarsen=coarsen_predict, data_vars=data_vars, mask=mask
+    store=store,
+    tlim=tlim,
+    coarsen=coarsen_predict,
+    variables=data_vars,
+    mask=mask,
+    sampling='monthly',
 )
-prediction = model.predict(x=climate[fit_vars], f=groups)
-ds['historical'] = integrated_risk(prediction['prob'] * coarsen_scale) * final_mask.values
+x, y = prepare.fire(climate, nftd, mtbs, add_local_climate_trends=True)
+x_z = utils.zscore_2d(x, mean=x_mean, std=x_std)
+yhat = model.predict(x_z)
+prediction = collect.fire(yhat, climate)
+ds['historical'] = (['time', 'y', 'x'], prediction['prediction'])
+ds = ds.assign_coords({'x': climate.x, 'y': climate.y, 'time': climate.time})
+account_key = os.environ.get('BLOB_ACCOUNT_KEY')
+if store == 'local':
+    ds.to_zarr('data/fire_historical.zarr', mode='w')
+elif store == 'az':
+    path = get_store('carbonplan-scratch', 'data/fire_historical.zarr', account_key=account_key)
+    ds.to_zarr(path, mode='w')
 
 print('[fire] evaluating on future climate')
-targets = list(map(lambda x: str(x), np.arange(2020, 2120, 20)))
-cmip_models = ['BCC-CSM2-MR', 'ACCESS-ESM1-5', 'CanESM5', 'MIROC6', 'MPI-ESM1-2-LR']
+cmip_models = [
+    ('CanESM5', 'r10i1p1f1'),
+    ('UKESM1-0-LL', 'r10i1p1f2'),
+    ('MRI-ESM2-0', 'r1i1p1f1'),
+    ('MIROC-ES2L', 'r1i1p1f2'),
+    ('MIROC6', 'r10i1p1f1'),
+    ('FGOALS-g3', 'r1i1p1f1'),
+    ('HadGEM3-GC31-LL', 'r1i1p1f3'),
+]
 scenarios = ['ssp245', 'ssp370', 'ssp585']
-for cmip_model in cmip_models:
+ds_future = xr.Dataset()
+for (cmip_model, member) in cmip_models:
     for scenario in tqdm(scenarios):
-        results = []
-        for target in targets:
-            tlim = (int(target) - 5, int(target) + 4)
+        try:
+            store = 'az'
+            results = []
             climate = load.cmip(
                 store=store,
                 model=cmip_model,
                 coarsen=coarsen_predict,
                 scenario=scenario,
-                tlim=tlim,
-                data_vars=data_vars,
+                tlim=('2015', '2099'),
+                variables=data_vars,
+                sampling='monthly',
+                member=member,
             )
-            prediction = model.predict(x=climate[fit_vars], f=groups)
-            results.append(integrated_risk(prediction['prob'] * coarsen_scale) * final_mask.values)
-        da = xr.concat(results, dim=xr.Variable('year', targets))
-        ds[cmip_model + '_' + scenario] = da
-        ds.to_zarr('data/fire.zarr', mode='w')
+            x, y = prepare.fire(climate, nftd, mtbs, add_local_climate_trends=False)
+            x_z = utils.zscore_2d(x, mean=x_mean, std=x_std)
+            y_hat = model.predict(x_z)
+            prediction = collect.fire(y_hat, climate)
+            ds_future[cmip_model + '_' + scenario] = prediction['prediction']
+            print('[fire] completed future run for {}-{}'.format(cmip_model, scenario))
+
+        except:
+            print(
+                "some things just don't work out in life, and {}-{} is one such example".format(
+                    cmip_model, scenario
+                )
+            )
+# assigning the coords as below makes it easier for follow-on analysis when binning by forest group type
+ds_future = ds_future.assign_coords({'x': nftd.x, 'y': nftd.y})
+if store == 'local':
+    ds_future.to_zarr('data/fire_future.zarr', mode='w')
+elif store == 'az':
+    store = get_store('carbonplan-scratch', 'data/fire_future.zarr', account_key=account_key)
+    ds_future.to_zarr(store, mode='w')
