46b11d99b818185292ffba3f74fce53284c3a03d
Oriana Chegwidden <orianac@uw.edu>
2021-03-23 00:25:59 +0000

    adding figure notebooks

---
+++ scripts/runs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+#python fire.py az tmean_ppt_true [tmean,ppt] 4 True
+#python fire.py az tmean_ppt_false [tmean,ppt] 4 False
+#python fire.py az tmean_ppt_cwd_true [tmean,ppt,cwd] 4 True
+#python fire.py az tmean_ppt_cwd_false [tmean,ppt,cwd] 4 False
+#python fire.py az tmean_ppt_cwd_pdsi_true [tmean,ppt,cwd,pdsi] 4 True
+#python fire.py az tmean_ppt_cwd_pdsi_false [tmean,ppt,cwd,pdsi] 4 False
+python fire.py az v2_remaining [tmean,ppt,cwd] 4
