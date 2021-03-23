3845578a6b8beaab9a6f94353c6507cba583e413
Oriana Chegwidden <orianac@uw.edu>
2021-03-23 17:54:01 +0000

    plotting notebook updates

--- scripts/runs.sh
+++ scripts/runs.sh
@@ -7,4 +7,5 @@
 #python fire.py az tmean_ppt_cwd_false [tmean,ppt,cwd] 4 False
 #python fire.py az tmean_ppt_cwd_pdsi_true [tmean,ppt,cwd,pdsi] 4 True
 #python fire.py az tmean_ppt_cwd_pdsi_false [tmean,ppt,cwd,pdsi] 4 False
-python fire.py az v2_remaining [tmean,ppt,cwd] 4
+#python fire.py az v2_remaining [tmean,ppt,cwd] 4
+python fire.py az v3_remaining [tmean,ppt,cwd] 4
