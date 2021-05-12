bf919b90391bd6554fef260f97f685e8646ad847
Oriana Chegwidden <orianac@uw.edu>
2021-05-12 21:04:02 +0000

    updates to article json file creation

--- scripts/article_prep.py
+++ scripts/article_prep.py
@@ -1,9 +1,11 @@
 # This script will prepare the variable timeseries for the web article.
 # TODO: temperature
 
+import json
 import os
 import warnings
 
+import fsspec
 import numpy as np
 import rioxarray
 import xarray as xr
@@ -12,6 +14,18 @@ from carbonplan_forest_risks import load, utils
 from carbonplan_forest_risks.utils import get_store
 
 # flake8: noqa
+
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
 
 
 warnings.filterwarnings('ignore')
@@ -27,8 +41,7 @@ website_mask = (
 region_bboxes = {
     'CONUS': {'x': slice(-3e6, 3e6), 'y': slice(4e6, 0)},
     'PNW': {'x': slice(-2.5e6, -1e6), 'y': slice(3.5e6, 2.4e6)},
-    'Southwest': {'x': slice(-1.8e6, 0.9e6), 'y': slice(1.8e6, 0.9e6)},
-    'Midwest': {'x': slice(-0.7e6, 0.5e6), 'y': slice(3e6, 1.2e6)},
+    'Southwest': {'x': slice(-1.8e6, -0.9e6), 'y': slice(1.8e6, 0.9e6)},
     'California': {'x': slice(-2.3e6, -1.8e6), 'y': slice(2.5e6, 1.2e6)},
     'Southeast': {
         'x': slice(0.6e6, 1.8e6),
@@ -38,12 +51,16 @@ region_bboxes = {
 
 results_dict = {}
 # select out bounding boxes
-for impact in ['insects', 'drought']:  # , 'fire', ]:
+for impact in ['fire', 'insects', 'drought']:
     results_dict[impact] = {}
+    if impact == 'fire':
+        store_path = 'risks/results/web/{}_cmip_high_res.zarr'.format(impact)
+    else:
+        store_path = 'risks/results/paper/{}_cmip.zarr'.format(impact)
     ds = xr.open_zarr(
         get_store(
             'carbonplan-forests',
-            'risks/results/paper/{}_cmip.zarr'.format(impact),
+            store_path,
             account_key=account_key,
         )
     )
@@ -60,6 +77,7 @@ for impact in ['insects', 'drought']:  # , 'fire', ]:
         ds = ds.groupby('time.year').sum().coarsen(year=10).mean().compute()
         ds = ds.assign_coords({'year': np.arange(1970, 2100, 10)})
     ds = ds.where(website_mask > 0)
+
     # then do rolling mean for two decades
     ds = ds.rolling(year=2).mean().drop_sel(year=1970)
     for region, bbox in region_bboxes.items():
@@ -68,7 +86,7 @@ for impact in ['insects', 'drought']:  # , 'fire', ]:
         if impact == 'fire':
             selected = selected.apply(utils.integrated_risk)
         else:
-            selected *= 20
+            selected *= 10
 
         selected = selected.mean(dim=['x', 'y']).compute().probability
         # first populate the historical values
@@ -76,10 +94,9 @@ for impact in ['insects', 'drought']:  # , 'fire', ]:
         # initialize your dictionary with the gcm keys
         for gcm in gcms:
             results_dict[impact][region]['historical']['models'][gcm] = []
-
+        print('at historical')
         for year in np.arange(1980, 2020, 10):
-            # doesn't matter which scenario you pick here- they're all the same
-            # fill in the mean
+            # average across scenarios
             results_dict[impact][region]['historical']['mean'].append(
                 {
                     'y': year,
@@ -92,14 +109,15 @@ for impact in ['insects', 'drought']:  # , 'fire', ]:
                     {'y': year, 'r': selected.mean(dim='scenario').sel(gcm=gcm, year=year).values}
                 )
 
-        for scenario, years in scenario_years.items():
+        for scenario in scenarios:
+            print(scenario)
             results_dict[impact][region][scenario] = {}
             results_dict[impact][region][scenario] = {'mean': [], 'models': {}}
             # initialize your dictionary with the gcm keys
             for gcm in gcms:
                 results_dict[impact][region][scenario]['models'][gcm] = []
             # loop through scenarios
-            for year in years:
+            for year in np.arange(2020, 2100, 10):
 
                 results_dict[impact][region][scenario]['mean'].append(
                     {
@@ -112,3 +130,11 @@ for impact in ['insects', 'drought']:  # , 'fire', ]:
                     results_dict[impact][region][scenario]['models'][gcm].append(
                         {'y': year, 'r': selected.sel(gcm=gcm, scenario=scenario, year=year).values}
                     )
+
+with fsspec.open(
+    'az://carbonplan-forests/risks/results/web/time-series.json',
+    account_name="carbonplan",
+    account_key=account_key,
+    mode='w',
+) as f:
+    json.dump(results_dict, f, indent=2, cls=NpEncoder)
