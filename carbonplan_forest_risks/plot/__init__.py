9892223f527f39641c48f080082762936372372e
Oriana Chegwidden <orianac@uw.edu>
2021-05-12 19:27:25 +0000

    ts plotting cleanup

--- carbonplan_forest_risks/plot/__init__.py
+++ carbonplan_forest_risks/plot/__init__.py
@@ -3,4 +3,4 @@ from .xy import xy  # isort:skip
 from .line import line  # isort:skip
 from .carto import carto, cartopy_borders, cartopy_proj_albers
 from .fire import evaluation, monthly, summary
-from .paper import add_colorbar, map_pretty
+from .paper import add_colorbar, map_pretty, multipanel_ts, ts_pretty
--- carbonplan_forest_risks/plot/paper.py
+++ carbonplan_forest_risks/plot/paper.py
@@ -1,4 +1,7 @@
 import matplotlib as mpl
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
 
 from . import cartopy_borders, cartopy_proj_albers
 
@@ -77,4 +80,92 @@ def add_colorbar(
     return cbar
 
 
-# def model_comparisons():
+def ts_pretty(ax, impact, ylims):
+    ax.set_xlim(1970, 2100)
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_ylim(ylims)
+    ax.set_title("")
+
+
+def multipanel_ts(results_dict, region_bboxes, fig_path):
+
+    gcms = [
+        ("MRI-ESM2-0", (0, 0)),
+        ("MIROC-ES2L", (1, 0)),
+        ("MPI-ESM1-2-LR", (2, 0)),
+        ("ACCESS-ESM1-5", (3, 0)),
+        ("ACCESS-CM2", (4, 0)),
+        ("CanESM5-CanOE", (5, 0)),
+    ]
+
+    titles = {
+        "fire": "Burn area\n(fraction/year)",
+        "drought": "Drought-related\nmortality (%/year)",
+        "insects": "Insect-related\nmortality (%/year)",
+    }
+    ylims = {
+        "fire": (0, 0.05),
+        "drought": (0, 2.5),
+        "insects": (0, 1),
+    }
+
+    fig = plt.figure(figsize=(12, 10))
+    full_gridspec = gridspec.GridSpec(4, 4, wspace=0.2, hspace=0.2)
+
+    for col, region in enumerate(region_bboxes.keys()):
+        ax = fig.add_subplot(full_gridspec[0, col], projection=cartopy_proj_albers())
+        map_pretty(ax, title=region)
+
+        ax.add_patch(
+            mpatches.Rectangle(
+                xy=[region_bboxes[region]['x'].start, region_bboxes[region]['y'].start],
+                width=region_bboxes[region]['x'].stop - region_bboxes[region]['x'].start,
+                height=region_bboxes[region]['y'].stop - region_bboxes[region]['y'].start,
+                facecolor='grey',
+                alpha=0.5,
+            )
+        )
+
+    for impact_num, impact in enumerate(["fire", "drought", "insects"]):
+        impact_axes = []
+        for j, region in enumerate(region_bboxes.keys()):
+            ax = fig.add_subplot(full_gridspec[1 + impact_num, j])
+
+            results_dict[impact][region]["historical"].plot(ax=ax, color="k", zorder=60)
+            for scenario in ["ssp245", "ssp370", "ssp585"]:
+                impact_axes.append(ax)
+                plot_future_ts_traces(ax, results_dict[impact][region]["future"], scenario, gcms)
+            ts_pretty(ax, impact, ylims[impact])
+            if impact != 'insects':
+                ax.set_xticks([])
+            if len(impact_axes) > 3:
+                ax.set_yticks([])
+            else:
+                ax.set_ylabel(titles[impact])
+
+    for format_string in ["svg", "png"]:
+        plt.savefig(fig_path + format_string, format=format_string)
+
+
+def plot_future_ts_traces(ax, ds, scenario, gcms):
+
+    scenario_colors = {
+        "ssp245": "#59A82F",
+        "ssp370": "#D8B525",
+        "ssp585": "#D83232",
+    }
+    scenario_colors_light = {
+        "ssp245": "#DEEED5",
+        "ssp370": "#F7F0D3",
+        "ssp585": "#F7D6D6",
+    }
+
+    ssp_rename = {"ssp245": "SSP2-4.5", "ssp370": "SSP3-7.0", "ssp585": "SSP5-8.5"}
+
+    for (gcm, location) in gcms:
+        ds.sel(gcm=gcm, scenario=scenario).plot(ax=ax, color=scenario_colors_light[scenario])
+
+    ds.sel(scenario=scenario).mean(dim="gcm").plot(
+        ax=ax, color=scenario_colors[scenario], label=ssp_rename[scenario], zorder=30
+    )
